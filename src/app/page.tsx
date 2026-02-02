bf3b4ad7e81dc8f4ddbcf50472d2a158e02cfd6b
rsyarsya <rasyadwaarsyairnantyanto2006@mail.ugm.ac.id>
2026-02-02 10:31:00 +0700

    feat: integrate updated sections into page and global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Header } from '@/components/home/Header';
 import { AboutSection } from '@/components/home/AboutSection';
+import { ProjectArea } from '@/components/home/ProjectArea';
+import { TeamMember } from '@/components/home/TeamMember';
+import { GoalsSection } from '@/components/home/GoalsSection';
 import { EmptySection } from '@/components/home/EmptySection';
 
 export default function Home() {
@@ -8,6 +11,9 @@ export default function Home() {
     <main>
       <Header />
       <AboutSection />
+      <ProjectArea />
+      <TeamMember />
+      <GoalsSection />
       <EmptySection />
     </main>
   );
